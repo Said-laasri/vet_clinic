56d2d6ae8d9391a64bbd20e46a51263a3dd4c9cf
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-12 17:48:47 +0100

    Create table sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE 'animals'(
+  `id` int(11) PRIMARY KEY NOT Null,
+  `name` varchar(255) NOT NULL,
+  `date_of_birth` smalldatetime NOT NULL,
+  `escape_attempts` smallint NOT NULL,
+  `neuterred` boolean NOT NULL,
+  `weight_kg` decimal NOT NULL ,
+); 
