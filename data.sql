64a99f95f1b54d0ace7508d0df3e586561da3567
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-12 17:50:39 +0100

    Add value to table

--- data.sql
+++ data.sql
@@ -1,2 +1,9 @@
 /* Populate database with sample data. */
 
+VALUES ('Agumon', '2020-05-30', 0, TRUE, 11.00);
+
+VALUES ('Gabumon', '2022-03-15', 3, FALSE, 15.50);
+
+VALUES ('Pikachu', '2023-01-07', 0, FALSE, 0.8);
+
+VALUES ('Devimon', '2022-05-12', 2, TRUE, 12.00);
