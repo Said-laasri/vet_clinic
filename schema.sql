aa97c471ad76d0eedfc8d53fcfa4577ccc888806
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-12 19:37:10 +0100

    Add queries sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-CREATE TABLE 'animal'(
-  `id` BIGSERIAL PRIMARY KEY NOT Null,
-  `name` varchar(255) NOT NULL,
-  `date_of_birth` DATE NOT NULL,
-  `escape_attempts` smallint ,
-  `neuterred` boolean ,
-  `weight_kg` decimal ,
+CREATE TABLE animal(
+  id BIGSERIAL PRIMARY KEY NOT Null,
+  name varchar(255) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts smallint ,
+  neuterred boolean ,
+  weight_kg decimal ,
 ); 
