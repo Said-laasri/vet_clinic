ff5ca10111faf6819c228dddc58ab43f8f4056a0
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-13 15:49:40 +0100

    Add screan shots file with photos

--- data.sql
+++ data.sql
@@ -13,5 +13,11 @@ INSERT INTO animal (name, date_of_birth, escape_attempts, neuterred, weight_kg)
 VALUES ('Devimon', date'2022-05-12', '5', 'TRUE', '11');
 
 INSERT INTO animal (name, date_of_birth, escape_attempts, neuterred, weight_kg)
-VALUEs('Charmander', date '2020-02-08', '0', 'TRUE','-11'),('Plantmon', date '2021-11-15', '2', 'TRUE', '-5.7'),('Squirtle', date '1993-04-02', '3', 'FALSE', '-12.13'),('Angemon', date '2005-06-12', '1', 'TRUE', '-45'),('Boarmon', date '2005-06-07', '7', 'TRUE', '20.4'),('Blossom', date '1998-10-13', '3', 'TRUE', '17'),('Ditto', date '2022-05-14','4','TRUE','22');
+VALUEs('Charmander', date '2020-02-08', '0', 'TRUE','-11'),
+('Plantmon', date '2021-11-15', '2', 'TRUE', '-5.7'),
+('Squirtle', date '1993-04-02', '3', 'FALSE', '-12.13'),
+('Angemon', date '2005-06-12', '1', 'TRUE', '-45'),
+('Boarmon', date '2005-06-07', '7', 'TRUE', '20.4'),
+('Blossom', date '1998-10-13', '3', 'TRUE', '17'),
+('Ditto', date '2022-05-14','4','TRUE','22');
 
